bcea7e536df1b62b29beda95c3f20cde0359ba9c
samstromer1990-crypto <samstromer1990@gmail.com>
2026-09-17 19:29:12 +0530

    Time: 3 ms (3.1%), Space: 19.5 MB (19.76%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,14 +1,15 @@
 class Solution:
     def plusOne(self, digits: list[int]) -> list[int]:
-        s = 0
-        for i in digits:
-            s = s * 10 + i
-            
-        s += 1
         
-        output = []
-        while s > 0:
-            output.append(s % 10)
-            s = s // 10
-            
-        return output[::-1]
+        def list_to_num(idx, current_num):
+            if idx == len(digits):
+                return current_num
+            return list_to_num(idx + 1, current_num * 10 + digits[idx])
+
+        def num_to_list(num):
+            if num == 0:
+                return []
+            return num_to_list(num // 10) + [num % 10]
+
+        total = list_to_num(0, 0) + 1
+        return num_to_list(total)
